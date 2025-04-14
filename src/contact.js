2afe015e2a470dd21cba8854485621a8e389a595
laiamanda <40805270+laiamanda@users.noreply.github.com>
2025-04-14 12:34:39 -0400

    Create contact.js

---
+++ src/contact.js
@@ -0,0 +1,27 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Peppa Pig",
+    imgURL:
+      "https://www.independent.ie/lifestyle/33525/30179868.ece/AUTOCROP/h530/HEA_2014-04-14_LIF_008_31207881_I1.JPG",
+    phone: "+123 456 789",
+    email: "bad-bish@canWhistle.com"
+  },
+  {
+    id: 2,
+    name: "Oli London",
+    imgURL: "https://static.smalljoys.me/2019/09/oli-london-instagram-5.jpg",
+    phone: "+987 654 321",
+    email: "realjimin@smallhit.en"
+  },
+  {
+    id: 3,
+    name: "Prince Philip",
+    imgURL:
+      "https://im-media.voltron.voanews.com/Drupal/01live-166/styles/sourced/s3/ap-images/2021/02/b896b29af5176c624c0fead5ce1a6873.jpg?itok=uP0BlIPX",
+    phone: "+918 372 574",
+    email: "imAlive@urRoyalty.com"
+  }
+];
+
+export default contacts;
